5073bb668c0e79c1ea7a0a8b9d03ea150172c7fe
pianocomposer321 <tmarklebw88@gmail.com>
2023-10-16 09:10:02 -0500

    initial commit
    No ui, algorithm in place. Saving changes before a refactor that uses
    ints and bitwise instead of tuples for directions.

---
+++ src/main.rs
@@ -0,0 +1,204 @@
+use std::collections::HashMap;
+
+use once_cell::sync::OnceCell;
+use rand::{rngs::ThreadRng, seq::SliceRandom, Rng};
+use thiserror::Error;
+
+const COLS: usize = 20;
+const ROWS: usize = 20;
+const DEFAULT_MAX_ITERATIONS: usize = 1_000_000;
+
+static WORDS: OnceCell<Vec<&'static str>> = OnceCell::new();
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Direction {
+    Horizontal,
+    Diagonal,
+    Vertical,
+}
+
+impl From<&Direction> for (usize, usize) {
+    fn from(value: &Direction) -> Self {
+        match &value {
+            Direction::Horizontal => (0, 1),
+            Direction::Diagonal => (1, 1),
+            Direction::Vertical => (1, 0),
+        }
+    }
+}
+
+type Board = Vec<Vec<char>>;
+
+fn print_board(board: &Board) {
+    for row in board.iter() {
+        println!(
+            "{}",
+            row.iter()
+                .map(|c| {
+                    let mut s = c.to_string();
+                    s.push(' ');
+                    s
+                })
+                .collect::<String>()
+        );
+    }
+}
+
+#[derive(Error, Debug)]
+enum GenerationError {
+    #[error("No possible board with this configuration. Increase board size.")]
+    NoPossibleBoard,
+    #[error("Reached maximum iterations. Increase iteration limit or board size.")]
+    MaxIterationsReached,
+}
+
+fn generate_board(
+    rows: usize,
+    cols: usize,
+    words: &Vec<&'static str>,
+    max_iterations: Option<usize>,
+) -> Result<Board, GenerationError> {
+    let mut iterations: usize = 0;
+    let mut rng = rand::thread_rng();
+
+    let mut directions_count: HashMap<(usize, usize), usize> = HashMap::from([
+            ((&Direction::Horizontal).into(), 0),
+            ((&Direction::Diagonal).into(), 0),
+            ((&Direction::Vertical).into(), 0),
+    ]);
+
+    fn generate_board_impl(
+        rows_count: usize,
+        cols_count: usize,
+        words: &Vec<&'static str>,
+        word_ind: usize,
+        board: Board,
+        iterations: &mut usize,
+        max_iterations: Option<usize>,
+        directions_count: &mut HashMap<(usize, usize), usize>,
+        rng: &mut ThreadRng,
+    ) -> Result<Board, GenerationError> {
+        let word = words[word_ind];
+        let mut directions = [
+            Direction::Horizontal,
+            Direction::Diagonal,
+            Direction::Vertical,
+        ];
+        directions.sort_by_key(|direction| directions_count[&(direction).into()]);
+        // directions.shuffle(rng);
+        for direction in directions
+            .iter()
+            .map(|direction| Into::<(usize, usize)>::into(direction))
+        {
+            let mut max_row = rows_count;
+            if direction.0 == 1 {
+                if word.len() > rows_count {
+                    continue;
+                }
+                max_row = rows_count - word.len();
+            }
+            if direction.1 == 1 && word.len() > rows_count {
+                continue;
+            }
+
+            let rand_initial_row = rng.gen_range(0..max_row);
+            for mut row_ind_for_first_letter in 0..rows_count {
+                let mut max_col = cols_count;
+                if direction.1 == 1 {
+                    if word.len() > cols_count {
+                        continue;
+                    }
+                    max_col = cols_count - word.len();
+                }
+
+                row_ind_for_first_letter = (row_ind_for_first_letter + rand_initial_row) % max_row;
+
+                let rand_initial_col = rng.gen_range(0..max_col);
+                for mut col_ind_for_first_letter in 0..cols_count {
+                    *iterations += 1;
+                    if let Some(max_iterations) = max_iterations {
+                        if *iterations > max_iterations {
+                            return Err(GenerationError::MaxIterationsReached);
+                        }
+                    }
+
+                    let mut board_copy = Vec::new();
+                    for row in board.iter() {
+                        board_copy.push(row.clone());
+                    }
+
+                    col_ind_for_first_letter = (col_ind_for_first_letter + rand_initial_col) % max_col;
+                    let mut succesful = true;
+
+                    let mut row_ind_for_current_letter = row_ind_for_first_letter;
+                    let mut col_ind_for_current_letter = col_ind_for_first_letter;
+                    for letter in word.chars() {
+                        if board[row_ind_for_current_letter][col_ind_for_current_letter] != ' ' && board[row_ind_for_current_letter][col_ind_for_current_letter] != letter {
+                            succesful = false;
+                            break;
+                        }
+                        board_copy[row_ind_for_current_letter][col_ind_for_current_letter] = letter;
+                        row_ind_for_current_letter += direction.0;
+                        col_ind_for_current_letter += direction.1;
+                    }
+                    if succesful {
+                        if word_ind + 1 == words.len() {
+                            return Ok(board);
+                        }
+
+                        directions_count.insert(direction, directions_count[&direction] + 1);
+
+                        if let Ok(board) = generate_board_impl(
+                            rows_count,
+                            cols_count,
+                            words,
+                            word_ind + 1,
+                            board_copy,
+                            iterations,
+                            max_iterations,
+                            directions_count,
+                            rng,
+                        ) {
+                            return Ok(board);
+                        } else {
+                            directions_count.insert(direction, directions_count[&direction] - 1);
+                            continue;
+                        };
+                    }
+                }
+            }
+        }
+        Err(GenerationError::NoPossibleBoard)
+    }
+
+    let generated = generate_board_impl(
+        rows,
+        cols,
+        words,
+        0,
+        vec![vec![' '; cols]; rows],
+        &mut iterations,
+        max_iterations,
+        &mut directions_count,
+        &mut rng,
+    );
+    dbg!(directions_count);
+    generated
+}
+
+fn main() -> anyhow::Result<()> {
+    WORDS
+        .set(include_str!("../words.txt").lines().collect())
+        .unwrap();
+
+    let words = WORDS.get().unwrap();
+
+    print_board(&generate_board(
+        ROWS,
+        COLS,
+        words,
+        Some(DEFAULT_MAX_ITERATIONS),
+    )?);
+
+    Ok(())
+}
